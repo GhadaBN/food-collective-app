2dfca074984859a00928712c7437c584ee300183
Ghada Bennasr <gh.bennasr@gmail.com>
2024-05-23 21:53:18 +0200

    header and navbar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="header-content">
+        <div className="img-container">
+          <img src="/header_text.png" className="header-img" />
+        </div>
+        <button>view Menu</button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;

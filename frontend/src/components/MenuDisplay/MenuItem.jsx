31b69e352981c613f42dd7b23c1c61d1089cd433
Ghada Bennasr <gh.bennasr@gmail.com>
2024-05-27 18:46:41 +0200

    cart page persistant items

--- frontend/src/components/MenuDisplay/MenuItem.jsx
+++ frontend/src/components/MenuDisplay/MenuItem.jsx
@@ -6,23 +6,30 @@ import { StoreContext } from "../../context/StoreContext";
 function MenuItem({ item, baseUrl }) {
   const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
   const itemId = item._id;
+
   return (
     <div className="menu-item-container">
-      <img src={`${baseUrl}/images/${item.image}`} />
+      <img src={`${baseUrl}/images/${item.image}`} alt={item.itemName} />
       {!cartItems[itemId] ? (
         <img
           className="add"
           onClick={() => addToCart(itemId)}
           src={assets.add_icon_white}
+          alt="Add"
         />
       ) : (
         <div className="item-counter">
           <img
             onClick={() => removeFromCart(itemId)}
             src={assets.remove_icon_red}
+            alt="Remove"
           />
           <p>{cartItems[itemId]}</p>
-          <img onClick={() => addToCart(itemId)} src={assets.add_icon_green} />
+          <img
+            onClick={() => addToCart(itemId)}
+            src={assets.add_icon_green}
+            alt="Add more"
+          />
         </div>
       )}
       <p className="item-name">{item.itemName}</p>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 function Cart() {
-  return <div>Cart</div>;
+  const { cartItems, removeFromCart, menuItems } = useContext(StoreContext);
+
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        {Object.entries(cartItems).map(([itemId, quantity]) => {
+          const item = menuItems.find((item) => item._id === itemId);
+          return item ? (
+            <div key={itemId}>
+              <p>{item.itemName}</p>
+              <p>${item.price}</p>
+              <p>{quantity}</p>
+              <p>${item.price * quantity}</p>
+              <button onClick={() => removeFromCart(itemId)}>Remove</button>
+            </div>
+          ) : null;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Cart;

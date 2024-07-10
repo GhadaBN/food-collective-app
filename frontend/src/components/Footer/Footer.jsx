d34135ffeacb04795e9978a712d5cd37df2e9932
Ghada Bennasr <gh.bennasr@gmail.com>
2024-07-10 15:39:50 +0200

    footer styling

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,34 +1,19 @@
 import React from "react";
 import "./Footer.css";
-import { Link } from "react-router-dom";
-import { assets } from "../../assets/assets";
+
 function Footer() {
   return (
     <div className="footer" id="footer">
       <div className="footer-content">
-        <div className="footer-content-left">
-          <p className="text-logo">Tomato Social Club</p>
-          <div className="social-icons">
-            <img src={assets.github_icon} />
-            <img src={assets.linkedin_icon} />
-          </div>
-        </div>
-        <div className="footer-content-center">
-          <p>Company</p>
+        <div className="footer-links">
           <ul>
-            <li>Home</li>
+            <li>Github</li>
+            <li>Linkedin</li>
             <li>About us</li>
-            <li>Delivery</li>
-            {/* <li>Privacy policy</li> */}
           </ul>
+          <hr className="footer-hr" />
         </div>
-        <div className="footer-content-right">
-          <p>Contact us</p>
-          <ul>
-            <li>contact@tomato.com</li>
-            <li>phone number</li>
-          </ul>
-        </div>
+        <p className="footer-button">Contact us</p>
       </div>
     </div>
   );

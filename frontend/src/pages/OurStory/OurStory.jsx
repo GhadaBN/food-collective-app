db401088a3806f1d0dd4a02d2657677d1963f160
Ghada Bennasr <gh.bennasr@gmail.com>
2024-07-11 19:01:36 +0200

    our story page

--- frontend/src/pages/OurStory/OurStory.jsx
+++ frontend/src/pages/OurStory/OurStory.jsx
@@ -10,11 +10,31 @@ const OurStory = () => {
         <div className="photo-container">
           <img className="responsive-image" src={assets.our_story} />
         </div>
-        <p className="description">Our Collective</p>
       </div>
-      <div className="right-container">
-        <div className="menu-wrapper"></div>
-        <div className="text"></div>
+      <div className="right-container-collective">
+        <div className="text-wrapper">
+          <p className="text-collective">
+            We are a passionate group dedicated to revolutionizing the way you
+            experience food delivery. Our story is rooted in a commitment to
+            sustainability, community, and equality.
+          </p>
+          <p className="text-collective">
+            Our collective is built on the principal of flat hierarchy which
+            fosters collaboration and shared responsibility, ensuring everyone's
+            voice is valued.
+          </p>
+          <p className="text-collective">
+            By choosing pedal power over fossil fuels, we minimize our
+            environmental impact and contribute to cleaner, greener streets.
+          </p>
+          <p className="text-collective">
+            We thrive on connections with our customers, partners, and each
+            other, believing in giving back and supporting local initiatives
+            that align with our mission. Together, we aim to deliver delicious
+            meals while making a positive impact on our community and the
+            environment.
+          </p>
+        </div>
       </div>
     </div>
   );

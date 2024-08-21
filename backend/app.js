1f638be83b15fd6b7f814251a1483b10c49983f1
Ghada Bennasr <gh.bennasr@gmail.com>
2024-08-21 16:20:58 +0200

    cors bug

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,11 @@ const app = express();
 require("./config")(app);
 app.use(
   cors({
-    origin: "https://tomato-social-frontend.onrender.com", // Specific origin
-    credentials: true, // to allow cookies to be shared between backend and frontend
+    origin: [
+      "https://tomato-social-frontend.onrender.com",
+      "http://localhost:5173",
+    ],
+    credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
